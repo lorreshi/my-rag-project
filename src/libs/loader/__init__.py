7ee69e5e853523732ee65fbd632f1a23e40ca5fb
lorreshi <lorre_shi@163.com>
2026-06-19 14:16:46 +0800

    feat(loader): add MarkdownLoader with local image parsing (T12)

--- src/libs/loader/__init__.py
+++ src/libs/loader/__init__.py
@@ -5,9 +5,17 @@ Importing this package registers the built-in loader implementations with the
 """
 from src.libs.loader.base_loader import BaseLoader
 from src.libs.loader.loader_factory import LoaderFactory, register_loader
+from src.libs.loader.markdown_loader import MarkdownLoader
 from src.libs.loader.pdf_loader import PdfLoader
 
 # Register built-in loaders with the factory so it works out of the box.
 register_loader([".pdf"], lambda: PdfLoader())
+register_loader([".md", ".markdown"], lambda: MarkdownLoader())
 
-__all__ = ["BaseLoader", "LoaderFactory", "register_loader", "PdfLoader"]
+__all__ = [
+    "BaseLoader",
+    "LoaderFactory",
+    "register_loader",
+    "PdfLoader",
+    "MarkdownLoader",
+]
